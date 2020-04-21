bbcae387c793c87799fbedd5670982df2991646a
Wes <wesleyayer92@gmail.com>
2020-04-21 11:00:20 -0400

    finishes printBox function

--- box.js
+++ box.js
@@ -1,5 +1,27 @@
 function printBox(height, width) {
+    let row = '';
+    let emptySpace = '';
 
+    // build row to width
+    for (let i = 0; i < width; i++) {
+        row += '*';
+    }
+
+    // print top row
+    console.log(row);
+
+    // build inner empty space
+    for (let j = 0; j < width - 2; j++) {
+        emptySpace += ' ';
+    }
+
+    // print hollow rows to height
+    for (let i = 0; i < height - 2; i++) {
+        console.log(`*${emptySpace}*`);
+    }
+
+    // print bottom row
+    console.log(row);
 }
 
 printBox(4, 6);
